b7ba46d0c7819aa07805d384f0e49e5ef8b3f7c3
Drew Wagner <drewm1980@gmail.com>
2010-03-10 15:24:00 -0600

    ValueError: too many values to unpack > /usr/lib/pymodules/python2.5/urwid/widget.py(1126)render()    1125         """ -> 1126         (maxcol,) = size    1127         self._shift_view_to_cursor = bool(focus)

--- nv.py
+++ nv.py
@@ -3,17 +3,50 @@
 # Andrew Wagner 2010
 import urwid
 
-filterEdit = urwid.Edit("This is the filter text.", wrap='clip')
-filterFiller = urwid.Filler( filterEdit )
+database = {'fruit':'apples\noranges\npears\npeaches\raspberries\strawberries',
+            'animals':'pythons\nbears\njellyfish\ndolphins\nelephants',
+            'Sustainable Means of Transportation':'bicycling\n\
+            \nwalking\n\
+            taking the train',
+            "A few of Drew's favorite things":'python programming language\n\
+            bicycling\nstrawberries\nnotational velocity\n'}
 
-def do_reply(input):
-    if input != 'enter':
-        return
-    if filterFiller.body == ask:
-        filterFiller.body = urwid.Text( "Filter text is...\n"+
-            ask.edit_text )
-    else:
-        raise urwid.ExitMainLoop()
+palette = [('filter', 'white', 'black'),
+    ('list focus', 'black', 'dark cyan', 'standout'),
+    ('list nofocus', 'black', 'dark gray'),
+    ('body', 'black', 'white')]
 
-loop = urwid.MainLoop(filterFiller, unhandled_input=do_reply)
+# An Edit box for the filter
+filterEdit = urwid.Edit("Filter?:", wrap='clip', multiline=False)
+filterMap = urwid.AttrMap(filterEdit, 'filter')
+
+# Temporary stand-in for what will probably be a listbox...
+listText = urwid.Text('')
+listMap = urwid.AttrMap(listText, 'list nofocus', 'list_focus')
+
+# An Edit box for the body of the note
+bodyEdit = urwid.Edit("Body?:")
+bodyMap = urwid.AttrMap(bodyEdit, 'body')
+
+def on_filter_change(widget, signal):
+    assert widget == filterEdit
+    assert type(signal) == str
+    newString = signal
+    listText.set_text('Got string: ' + newString)
+
+urwid.connect_signal(filterEdit, 'change', on_filter_change)
+
+pile = urwid.Pile([('fixed', 1, filterMap),
+    ('weight',1,listMap),
+    ('weight',2,bodyMap)], focus_item=filterMap)
+
+def do_pass():
+    pass
+
+def do_quit():
+    raise urwid.ExitMainLoop()
+
+#loop = urwid.MainLoop(listbox, palette, unhandled_input=on_unhandled_input)
+loop = urwid.MainLoop(pile, palette, unhandled_input=do_pass)
+
 loop.run()
